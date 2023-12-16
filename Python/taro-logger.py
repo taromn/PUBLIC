ae87c2df8d9315800104247d2106059c76a8a4bb
taromn <54003207+taromn@users.noreply.github.com>
2023-12-17 03:32:23 +0900

    add handler

--- Python/taro-logger.py
+++ Python/taro-logger.py
@@ -1,17 +1,29 @@
 
 import logging
 import time
+import sys
 
-logging.basicConfig(filename='output.log', encoding='utf-8', level=logging.WARN)
+logging.basicConfig(filename='output.log', encoding='utf-8', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 def log_out(number):
-  logging.debug(f'DEBUG log count:{number}')
-  logging.info(f'INFO log count:{number}')
-  logging.warning(f'WARN log count:{number}')
-  logging.error(f'ERROR log count:{number}')
+  logger.debug(f'log count:{number}')
+  logger.info(f'log count:{number}')
+  logger.warning(f'log count:{number}')
+  logger.error(f'log count:{number}')
 
 
 if __name__ == "__main__":
   for i in range(100):
     log_out(i)
     time.sleep(1)
+
+
+# Reference
+# https://stackoverflow.com/questions/14058453/making-python-loggers-output-all-messages-to-stdout-in-addition-to-log-file
+# https://docs.python.org/3/howto/logging.html
+# https://www.datadoghq.com/blog/python-logging-best-practices/
