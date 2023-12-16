9e8f64e5ca92e6a63a96af38f2e4ea0479f358c5
taromn <54003207+taromn@users.noreply.github.com>
2023-12-17 04:15:53 +0900

    taro-logger.py: add rotation, log format

--- Python/taro-logger.py
+++ Python/taro-logger.py
@@ -2,13 +2,29 @@
 import logging
 import time
 import sys
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(filename='output.log', encoding='utf-8', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+format_a = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
+
+# stdout
+handler_std = logging.StreamHandler(sys.stdout)
+handler_std.setLevel(logging.DEBUG)
+handler_std.setFormatter(format_a)
+logger.addHandler(handler_std)
+
+# file out, rotation
+handler_rot = RotatingFileHandler(
+  'output.log',
+  maxBytes=10*1024,
+  backupCount=2,
+  encoding='utf-8'
+)
+handler_rot.setFormatter(format_a)
+logger.addHandler(handler_rot)
+
+logger.setLevel(logging.DEBUG)
 
 def log_out(number):
   logger.debug(f'log count:{number}')
